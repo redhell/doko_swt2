1293b3992caf97a6b95adc11577241a567bb3365
mikail <PC@192.168.0.129>
2018-05-21 21:10:14 +0200

    Methoden zum Entfernen hinzugefügt

--- doko/src/client/backend/model/classes/User.java
+++ doko/src/client/backend/model/classes/User.java
@@ -1,11 +1,13 @@
 package client.backend.model.classes;
 
-import java.net.Socket;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class User {
 
 	private String username;
 	private int score;
+	private JSONArray karten;
 	
 	public User(String username) {
 		this.username=username;
@@ -17,5 +19,37 @@ public class User {
 	
 	public int getScore() {
 		return score;
+	}
+	
+	public JSONArray getKarten() {
+		return karten;
+	}
+	
+	public void setKarten(JSONArray karten) {
+		this.karten = karten;
+	}
+	
+	public void entferneKarte(int index) {
+		karten.remove(index);
+	}
+	
+	public void entferneKarte(String symbol, String wertigkeit) {
+		for(int i=0; i<karten.length(); i++) {
+			JSONObject json = karten.getJSONObject(i);
+			if(symbol.equals(json.getString("symbol")) && wertigkeit.equals(json.getString(wertigkeit))) {
+				karten.remove(i);
+				break;
+			}
+		}
+	}
+	
+	public void entferneKarte(JSONObject job) {
+		for(int i=0; i<karten.length(); i++) {
+			JSONObject json = karten.getJSONObject(i);
+			if(json.equals(job)) {
+				karten.remove(i);
+				break;
+			}
+		}
 	}
 }
